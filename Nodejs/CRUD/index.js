1702fec3e8b2346aaed906a60497bfe251c67f28
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2024-02-07 10:54:26 -0300

    Add API CRUD Nodejs

---
+++ Nodejs/CRUD/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import usersRoutes from "./routes/usuarios.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use("/usuarios", usersRoutes);
+app.get("/", (req, res) => res.send("Bem vindo a API de Usuários"));
+app.all("*", (req, res) =>res.send("Essa página não existe."));
+
+app.listen(PORT, () =>console.log(`Servidor executando em: http://localhost:${PORT}`));
