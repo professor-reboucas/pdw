36ed80665b3864efc84925132661c6bd34a3780d
leonardoreboucas <leonardo_r_carvalho@yahoo.com.br>
2024-03-10 19:23:12 -0300

    Fontend com componentes do MUI

---
+++ Nodejs/frontend/src/views/usuario/Form.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+function UsuarioForm(props) {
+    const { carregarUsuarios, usuarioEmEdicao, UsrSrv, setUsuarioEmEdicao } = props;
+    const salvar = () => {
+        if (usuarioEmEdicao.novo){
+            delete usuarioEmEdicao.novo;
+            UsrSrv.add(usuarioEmEdicao);
+        } else {
+            UsrSrv.update(usuarioEmEdicao.atual, usuarioEmEdicao);
+        }
+        carregarUsuarios();
+        setUsuarioEmEdicao(false)
+    }
+
+    return (
+        usuarioEmEdicao && (
+            <Grid>
+                    <TextField 
+                        required={true}
+                        fullWidth={true}
+                        id="outlined-basic" 
+                        label="Nome" 
+                        variant="outlined" 
+                        onChange={(event) => {
+                            setUsuarioEmEdicao({...usuarioEmEdicao,nome:event.target.value});
+                        }}
+                        value={usuarioEmEdicao.nome}
+                    />
+                    <TextField 
+                        required={true}
+                        fullWidth={true}
+                        id="outlined-basic" 
+                        label="Idade" 
+                        variant="outlined" 
+                        onChange={(event) => {
+                            setUsuarioEmEdicao({...usuarioEmEdicao,idade:event.target.value});
+                        }}
+                        value={usuarioEmEdicao.idade}
+                    />
+                    <TextField 
+                        required={true}
+                        fullWidth={true}
+                        id="outlined-basic" 
+                        label="Estado" 
+                        variant="outlined" 
+                        onChange={(event) => {
+                            setUsuarioEmEdicao({...usuarioEmEdicao,estado:event.target.value});
+                        }}
+                        value={usuarioEmEdicao.estado}
+                    />
+
+                    <TextField 
+                        required={true}
+                        fullWidth={true}
+                        id="outlined-basic" 
+                        label="Cidade" 
+                        variant="outlined" 
+                        onChange={(event) => {
+                            setUsuarioEmEdicao({...usuarioEmEdicao,cidade:event.target.value});
+                        }}
+                        value={usuarioEmEdicao.cidade}
+                    />
+                <Button variant="contained" color="success" onClick={salvar}>Salvar</Button>
+            </Grid>
+        )
+  )
+}
+
+export default UsuarioForm;
---
+++ Nodejs/frontend/src/views/usuario/List.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+function UsuarioList(props) {
+  const { listagem, setUsuarioEmEdicao, carregarUsuarios, UsrSrv } = props;
+  
+  const removeUsuario = (item) => {
+    UsrSrv.remove(item);
+    carregarUsuarios();
+  }
+  
+  return (
+      <Card>
+        <CardContent>
+          {
+            (listagem.length === 0) ? (
+              <p>Nenhum usuário encontrado</p>
+            )
+            :
+            <Grid container spacing={2} p={3}>
+              <Grid item xs={3}>
+                <b>Nome</b>
+              </Grid>
+              <Grid item xs={1}>
+                <b>Idade</b>
+              </Grid>
+              <Grid item xs={2}>
+                <b>Estado</b>
+              </Grid>
+              <Grid item xs={3}>
+                <b>Cidade</b>
+              </Grid>
+              <Grid item xs={3}>
+                <b>Ações</b>
+              </Grid>
+            {
+              (listagem.map((val, key) => (
+                <React.Fragment key={key}>
+                  <Grid item xs={3}>
+                    <p>{val.nome}</p>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <p>{val.idade}</p>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <p>{val.estado}</p>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <p>{val.cidade}</p>
+                  </Grid>
+                  <Grid item xs={3} spacing={50}>
+                      <Button size="small" variant="contained" onClick={()=>setUsuarioEmEdicao({...val, atual:val})} color="warning">Alterar</Button>
+                      <Button size="small" variant="contained" onClick={()=>removeUsuario(val)} color="error">Excluir</Button>
+                  </Grid>
+                </React.Fragment>
+              )))
+            }
+            </Grid>
+          }
+        </CardContent>
+      </Card>
+  )
+}
+
+export default UsuarioList;
